7217230380bd4985bed6386315bb7dfa03b399ef
Okogun Celina Blessing <cokogun@gmail.com>
2022-07-12 11:48:59 +0000

    Team Work main.h

--- main.h
+++ main.h
@@ -51,3 +51,13 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
